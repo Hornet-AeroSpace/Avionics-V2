2e87b9f087d411c231655c8b91223112ba8974a1
andrerhenry <kinghenry238@gmail.com>
2025-01-19 23:21:26 -0800

    updated aero functions

--- Filters/KF_linearized.py
+++ Filters/KF_linearized.py
@@ -1,24 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import pi
+from numpy import pi, exp, sign
 
 
 def _cross_sect_area_calc(diameter: float) -> float:
-    """Calcualte the cross sectional area
-
-    Args:
-        diameter (float): Diameter of Rocket - meters
-
-    Returns:
-        float: Cross sectional aera - meters^2
-    """        
+   
     return pi * (diameter / 2) ** 2
+
+def F_aero_drag(drag_coefficient: float, cross_sect_area: float, altitude: float) -> float:
+    density = air_density(altitude)
+    f_aero_drag = drag_coefficient* 0.5 * density * cross_sect_area
+    return f_aero_drag
+
+def air_density(altitude: float) -> float:
+    beta = 0.1354/1000.0 # Density Constant - confrim Constant
+    density_sealevel = 1.225 # kg/m^3 at sea level
+    density = density_sealevel * exp(-beta*altitude)
+    return density
 
 gravity = 9.81 # m/s**2
 air_density_0 = 1.225 # kg/m^3 at sea level
 drag_coefficient = 0.4
 cross_sect_area = _cross_sect_area_calc(0.155) # m^2
 mass_intital = 30000/1000 # kg
+
+
 
 
 F = np.array([
@@ -31,4 +37,5 @@ G = np.array([
     [-gravity]
 ])
 
-print(F, G)
+print(F)
+print(G)
