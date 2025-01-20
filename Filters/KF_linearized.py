29b101407d59be04f224ae9398c6d899088bb813
andrerhenry <kinghenry238@gmail.com>
2025-01-20 09:53:50 -0800

    added example force equation

--- Filters/KF_linearized.py
+++ Filters/KF_linearized.py
@@ -18,6 +18,44 @@ def air_density(altitude: float) -> float:
     density = density_sealevel * exp(-beta*altitude)
     return density
 
+def mass(mass_inital):
+    # place holder for change in mass
+    return mass_inital
+
+# Main differential equation 
+def derivative(t: float, state: np.array, rocket: RocketConfig, motor: Motor) -> np.ndarray:
+    """State space equation to be integrated numericaly. 
+
+    Args:
+        t (float): Time of current step in integratoin - seconds
+        state (np.array): State Vector [altitude - m, velocity - m/s, mass -kg]
+        rocket (RocketConfig): RocketConfig class containing perameters/methods of rocket
+        motor (Motor): Motor class conatining peramters/methods of the motor
+
+    Returns:
+        np.ndarray: State array dervivative to be integrated [altitude_dot - m/s, velcoity_dot - m/s**2, mass_dot - kg/s]
+    """    
+    GRAVITY = 9.81 # m/s^2 change in gravity considered negligible
+    aero = Aero(rocket)
+
+    # State vector
+    altitude = state[0]
+    velocity = state[1]
+    mass = state[2]
+    
+    # Forces
+    f_gravity = GRAVITY * mass
+    f_aero = aero.F_aero_drag(velocity, altitude)
+    f_thrust, mass_dot = motor.motor_output(t)
+    
+    f_net = f_thrust - f_aero - f_gravity
+    acceleration = f_net/mass
+    
+    state_dot = np.array([velocity, acceleration, mass_dot])
+    
+    return state_dot
+
+
 gravity = 9.81 # m/s**2
 air_density_0 = 1.225 # kg/m^3 at sea level
 drag_coefficient = 0.4
@@ -25,11 +63,11 @@ cross_sect_area = _cross_sect_area_calc(0.155) # m^2
 mass_intital = 30000/1000 # kg
 
 
-
+alpha = F_aero_drag(drag_coefficient, cross_sect_area,)
 
 F = np.array([
     [1, 1],
-    [0, 1]
+    [0, (-gravity)]
 ])
 
 G = np.array([
