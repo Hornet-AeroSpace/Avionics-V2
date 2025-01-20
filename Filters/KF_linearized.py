a5dab2cad056e07828c0aebfeb6ed6bea88747ad
andrerhenry <kinghenry238@gmail.com>
2025-01-20 10:43:05 -0800

    renamed cross sect calc

--- Filters/KF_linearized.py
+++ Filters/KF_linearized.py
@@ -39,7 +39,7 @@ def new_velocity(velocity: float, time_step: float, drag_coefficient: float, dia
 gravity = 9.81 # m/s**2
 air_density_0 = 1.225 # kg/m^3 at sea level
 drag_coefficient = 0.4
-cross_sect_area = _cross_sect_area_calc(0.155) # m^2
+cross_sect_area = cross_sect_area_calc(0.155) # m^2
 mass_intital = 30000/1000 # kg
 
 
