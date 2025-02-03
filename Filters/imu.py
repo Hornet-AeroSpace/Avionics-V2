7051124b72c5309660ccd54b53482042421d3c29
andrerhenry <kinghenry238@gmail.com>
2025-02-02 16:05:46 -0800

    changed sigma to full name, standard deviation

--- Filters/imu.py
+++ Filters/imu.py
@@ -5,8 +5,8 @@ class imu:
     def __init__(self, noise_density, sample_rate):
         self.noise_density = noise_density # be sure to check units when entering 
         self.sample_rate = sample_rate
-        self.sigma = self.sigma_calc()
+        self.standard_deviations = self.standard_deviation_calc()
     
-    def sigma_calc(self):
-        sigma = self.noise_density * math.sqrt(self.sample_rate) * self.GRAVITY 
-        return sigma
+    def standard_deviation_calc(self):
+        standard_deviation = self.noise_density * math.sqrt(self.sample_rate) * self.GRAVITY 
+        return standard_deviation
