b23a7df215d44c0b207900afbbbc4e3a08c046db
Andre Henry <ahenry@pop-os.attlocal.net>
2025-02-03 22:17:15 -0800

    corrected file name

--- Filters/atm_properties.py
+++ Filters/atm_properties.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Air density data taken from http://www.braeunig.us/space/atmos.htm
 
-data = np.genfromtxt("Filters/atmospheric_density.csv", delimiter=",", skip_header=1)
+data = np.genfromtxt("Filters/atmospheric_properties.csv", delimiter=",", skip_header=1)
 
 def atmospheric_density(altitude: float):
     return np.interp(altitude, data[:,0], data[:,3])
